650ad03cf70be7fdbf26e6f89db6f4d60241cabb
Adrian Palomares <adrianpal45@gmail.com>
2020-10-07 17:33:40 -0700

    added initial form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-app';
+  applicationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.applicationForm = this.fb.group({
+      firstname: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.applicationForm.value);
+  }
 }
