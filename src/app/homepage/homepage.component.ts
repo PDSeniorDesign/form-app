2d1714918c6a03f3484c23e982346646dabed471
Adrian Palomares <adrianpal45@gmail.com>
2020-11-04 10:16:31 -0800

    Form arrays now include enum values

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -20,16 +20,38 @@ export class HomepageComponent implements OnInit {
   }
 
   //forms that pertains to either EMPLOYEE or CONTRACTOR and represented as values
-  employeeApplicationList: string[] = [
-    'ISD_ACTIVE_DIRECTORY_HOSTED_REGISTRATION_FORMS_PERMANENT_EMPLOYEES',
-    'ISD_Downey_Data_Center_Registration_LA_County_Employees',
-    'ISD_Internet_Registration_Form_Permanent_Employees',
+  employeeApplicationList = [
+    {
+      text:
+        'ISD Active Directory Hosted Registration Forms Permanent Employees',
+      value:
+        FormType.ISD_ACTIVE_DIRECTORY_HOSTED_REGISTRATION_FORMS_PERMANENT_EMPLOYEES,
+    },
+    {
+      text: 'ISD Downey Data Center Registration LA County Employees',
+      value: FormType.ISD_DOWNEY_DATA_CENTER_REGISTRATION_LA_COUNTY_EMPLOYEES,
+    },
+    {
+      text: 'ISD Internet Registration Form Permanent Employees',
+      value: FormType.ISD_INTERNET_REGISTRATION_FORM_PERMANENT_EMPLOYEES,
+    },
   ];
 
-  vendorApplicationList: string[] = [
-    'ISD Active Directory Hosted Registration Forms',
-    'ISD Downey Data Center Registration LA County',
-    'ISD Internet Registration Form',
+  vendorApplicationList = [
+    {
+      text: 'ISD Active Directory Hosted Registration Forms',
+      value:
+        FormType.ISD_ACTIVE_DIRECTORY_HOSTED_REGISTRATION_FORMS_CONTRACTOR_AND_VENDORS,
+    },
+    {
+      text: 'ISD Downey Data Center Registration LA County',
+      value:
+        FormType.ISD_DOWNEY_DATA_CENTER_REGISTRATION_CONTRACTORS_AND_AND_VENDORS,
+    },
+    {
+      text: 'ISD Internet Registration Form',
+      value: FormType.ISD_INTERNET_REGISTRATION_FORM_CONTRACTORS_AND_VENDORS,
+    },
   ];
 
   constructor(private router: Router) {}
@@ -58,5 +80,8 @@ export class HomepageComponent implements OnInit {
 
   printValues(): void {
     console.log(this.formName.value);
+    console.log(
+      FormType.ISD_ACTIVE_DIRECTORY_HOSTED_REGISTRATION_FORMS_CONTRACTOR_AND_VENDORS
+    );
   }
 }
