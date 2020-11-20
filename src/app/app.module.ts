1f5f7e556f81b317441a457343645b2e63c057e1
PaulClefUbeCoding <72354974+PaulClefUbeCoding@users.noreply.github.com>
2020-11-20 05:11:01 -0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HttpClientModule } from '@angular/common/http';
+import { CommunicationService } from './service/communication.service';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -46,8 +48,9 @@ import { InformationComponent } from './form/information/information.component';
     MatStepperModule,
     MatInputModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [CommunicationService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
