98cff551cb2b710e837cec822b7a472a9be7142a
Adrian Palomares <adrianpal45@gmail.com>
2021-01-26 22:34:42 -0800

    can render based on checkbox state

--- src/app/form/employee-form/access-information/access-information.component.spec.ts
+++ src/app/form/employee-form/access-information/access-information.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { EmployeeFormComponent } from '../employee-form.component';
 
 import { AccessInformationComponent } from './access-information.component';
 
@@ -8,9 +10,8 @@ describe('AccessInformationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AccessInformationComponent ]
-    })
-    .compileComponents();
+      declarations: [AccessInformationComponent, EmployeeFormComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +22,62 @@ describe('AccessInformationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // TODO: Test other checkboxes
+  it('should trigger function on checkbox click (IBM DATA CENTER)', () => {
+    spyOn(component, 'checkboxUpdate').and.callThrough();
+    // IBM Data Center Access element
+    const ibmDataCenterAccessCheckbox = fixture.debugElement.query(
+      By.css('input#ibmCheckbox')
+    ).nativeElement;
+
+    expect(ibmDataCenterAccessCheckbox.checked).toBeFalsy();
+
+    // Simulating a click
+    ibmDataCenterAccessCheckbox.click();
+    fixture.detectChanges();
+
+    expect(ibmDataCenterAccessCheckbox.checked).toBeTruthy();
+    expect(component.checkboxUpdate).toHaveBeenCalled();
+  });
+
+  it('should render the form on click (IBM DATA CENTER)', () => {
+    expect(component.renderIBMForm).toBeFalse();
+    // Container where forms will render
+    const accessInformationFormsDiv = fixture.debugElement.query(
+      By.css('div.accessInformationForms')
+    ).nativeElement;
+
+    // Make sure it is not rendered
+    expect(fixture.debugElement.query(By.css('.ibmForm'))).toBeFalsy();
+
+    const ibmDataCenterAccessCheckbox = fixture.debugElement.query(
+      By.css('input#ibmCheckbox')
+    ).nativeElement;
+
+    ibmDataCenterAccessCheckbox.click();
+    fixture.detectChanges();
+
+    expect(component.renderIBMForm).toBeTrue();
+    expect(accessInformationFormsDiv.hasChildNodes()).toBeTrue();
+    expect(fixture.debugElement.query(By.css('.ibmForm'))).toBeTruthy(); // Should be rendered
+  });
+
+  it('should unrender when reclicked', () => {
+    // renderIBMForm will be false at first
+
+    // Reclick
+    const ibmDataCenterAccessCheckbox = fixture.debugElement.query(
+      By.css('input#ibmCheckbox')
+    ).nativeElement;
+
+    ibmDataCenterAccessCheckbox.click();
+    ibmDataCenterAccessCheckbox.click();
+
+    fixture.detectChanges();
+    // Check that form is gone
+    expect(component.renderIBMForm).toBeFalse();
+    expect(fixture.debugElement.query(By.css('.ibmForm'))).toBeFalsy();
   });
 });
--- src/app/form/employee-form/access-information/access-information.component.ts
+++ src/app/form/employee-form/access-information/access-information.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-access-information',
   templateUrl: './access-information.component.html',
-  styleUrls: ['./access-information.component.css']
+  styleUrls: ['./access-information.component.css'],
 })
 export class AccessInformationComponent implements OnInit {
-
-  constructor() { }
+  renderIBMForm: boolean;
+  constructor() {}
 
   ngOnInit(): void {
+    this.renderIBMForm = false;
   }
 
+  checkboxUpdate(e): void {
+    // console.log('hello');
+    switch (e.target.id) {
+      case 'ibmCheckbox':
+        // Will update variable with checkbox
+        this.renderIBMForm = e.target.checked;
+        break;
+      default:
+        console.log('default case');
+    }
+  }
 }
