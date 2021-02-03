5601e1208c0395d18c25b5e444133c6be09ef5ba
rtiare <ttorosa55@gmail.com>
2021-02-02 23:35:35 -0800

    Refactored status service

--- src/app/core/services/status.service.ts
+++ src/app/core/services/status.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatusService {
-
+  apiUrl: string = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
-  public searchGet(url: string, options?: any) {
-    return this.http.get(url, options);
-}
+  public searchGetID(id: any) {
+    return this.http.get(`${this.apiUrl}/service_requests/${id}`);
+  }
 }
