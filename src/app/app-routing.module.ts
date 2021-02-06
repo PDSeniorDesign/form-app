4319bbec0f7126e687a4975bbe896da09c6ce316
rtiare <ttorosa55@gmail.com>
2021-02-06 03:44:25 -0800

    added routes to login/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,27 @@ import { ContractorFormComponent } from './form/contractor-form/contractor-form.
 import { EmployeeFormComponent } from './form/employee-form/employee-form.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RequestStatusComponent } from './request-status/request-status.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {
-    path: '',
+
+  { path:'login',
+    component: LoginComponent,
+  },
+  { path: 'register',
+    component: RegisterComponent,
+  },
+  { path: '',
     component: HomepageComponent,
   },
-  {
-    path: 'employee-form',
+  {path: 'employee-form',
     component: EmployeeFormComponent,
   },
-  {
-    path: 'contractor-form',
+  { path: 'contractor-form',
     component: ContractorFormComponent,
   },
-  {
-    path: 'request-status',
+  { path: 'request-status',
     component: RequestStatusComponent,
   },
 ];
