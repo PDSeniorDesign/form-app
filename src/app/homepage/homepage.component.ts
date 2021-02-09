4774133a2b87ebd28ee82820f7630650f4d9e78e
Adrian Palomares <adrianpal45@gmail.com>
2021-02-08 20:11:29 -0800

    added some animations and styling

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { animate, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(
+          '400ms',
+          style({
+            opacity: 1,
+          })
+        ),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(
+          '400ms',
+          style({
+            opacity: 0,
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomepageComponent implements OnInit {
   makingFormChoice: boolean;
