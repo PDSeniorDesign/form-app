0342b6db79c00de21817a7f5159c20e8f15bd386
Adrian Palomares <adrianpal45@gmail.com>
2021-02-09 19:01:44 -0800

    renders based on value of stepCounter

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -36,9 +36,34 @@ describe('HomepageComponent', () => {
   it('should have stepCounter variable and should be set to 0', () => {
     expect(component.stepCounter).toBe(0);
   });
-  it('should render the homepage as default', () => {
+  it('should render the homepage as default (step 0)', () => {
     const jumbotronTitle = fixture.debugElement.query(By.css('h1.display-4'))
       .nativeElement;
     expect(jumbotronTitle.innerText).toContain('Office of Public Defender');
+  });
+  it('should render the user selection when step is 1', () => {
+    // Set the counter to 1, render user selection step
+    component.stepCounter = 1;
+    fixture.detectChanges();
+
+    // Grab an element to test if it rendered
+    const testText = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(testText.innerText).toContain('user selection step works!');
+  });
+  it('should render the form type selection, whether new or continue form, when stepCounter is 2', () => {
+    // Set the stepcounter to two
+    component.stepCounter = 2;
+    fixture.detectChanges();
+
+    // grab element to check that it rendered
+    const testText = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(testText.innerText).toContain('form type selection works!');
+  });
+  it('should render the request number prompt if the stepCounter is 3', () => {
+    component.stepCounter = 3;
+    fixture.detectChanges();
+
+    const testText = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(testText.innerText).toContain('request number prompt works');
   });
 });
