9ce69d7ee4d09f302740d8bbfb9e1b1679d50aca
Adrian Palomares <adrianpal45@gmail.com>
2021-02-17 14:46:06 -0800

    If user saved before submitting, same form will be used

--- src/app/form/employee-form/submit-page/submit-page.component.ts
+++ src/app/form/employee-form/submit-page/submit-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ApiHttpService } from 'src/app/core/services/api-http.service';
+import { FormDataService } from 'src/app/core/services/form-data.service';
 
 @Component({
   selector: 'app-submit-page',
@@ -9,19 +10,39 @@ import { ApiHttpService } from 'src/app/core/services/api-http.service';
 export class SubmitPageComponent implements OnInit {
   @Input() regForm;
   @Input() setSubmitResponse; // Function to update parent (employee-form.component)
-  constructor(private apiHttpService: ApiHttpService) {}
+  constructor(
+    private apiHttpService: ApiHttpService,
+    private formDataService: FormDataService
+  ) {}
 
   ngOnInit(): void {}
 
   onClick(): void {
     console.log(this.regForm.value); //debugging
-    this.apiHttpService
-      .createForm((this.regForm.value))
-      .subscribe((res) => {
+    // If there is a form in formData then there is a form in progress
+    if (this.formDataService.formData != undefined) {
+      // Save the form
+      this.apiHttpService
+        .saveForm(
+          this.formDataService.formData.requestNumber,
+          this.regForm.value
+        )
+        .subscribe((res) => {
+          console.log(res);
+          // Set the formData to the response, might be needed somewhere else
+          this.formDataService.formData = res;
+
+          // Set the submitResponse so that the submit page renders
+          this.setSubmitResponse(res);
+        });
+
+      // If the else statement executes, then the user probably didn't save their progress
+      // as they were completing the form
+    } else {
+      this.apiHttpService.createForm(this.regForm.value).subscribe((res) => {
         console.log(res);
         this.setSubmitResponse(res);
       });
+    }
   }
-
-
 }
