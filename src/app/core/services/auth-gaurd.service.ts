4604989bb629be8f8eeb6d61e8282e857dc90ba7
rtiare <ttorosa55@gmail.com>
2021-03-01 10:39:49 -0800

    Created auth service

---
+++ src/app/core/services/auth-gaurd.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { StatusService } from './status.service';
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+constructor(public zone: NgZone, public router: Router, private statusService: StatusService) {
+
+}
+canActivate(): boolean {
+if (!Helper.isNextStep) {
+  this.zone.run(() => {
+    this.router.navigate(['']) //you can redirect user to any page here ( Optional )
+  })
+  return false;  //block navigation
+}
+else {
+  return Helper.isNextStep || true;  // allow navigation
+}
+}
