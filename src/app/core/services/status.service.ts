086ac7f3758692db6ec3c2dfd3f1c912ec75eef3
rtiare <ttorosa55@gmail.com>
2021-03-01 10:06:10 -0800

    Added new functions to users

--- src/app/core/services/status.service.ts
+++ src/app/core/services/status.service.ts
@@ -1,15 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StatusService {
+
+  //block url
+  static isNextStep: boolean;
   apiUrl: string = environment.apiUrl;
+  //hold password for admin
+  public adminPassword: any;
   constructor(private http: HttpClient) {}
 
   public searchById(id: any) {
-    return this.http.get(`${this.apiUrl}/request_statuses/${id}`);
+    return this.http.get(`${this.apiUrl}/admin/${this.adminPassword}/service_requests/${id}`);
+  }
+
+  //display request into observable array
+  public display(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/admin/${this.adminPassword}/service_requests`);
+  }
+
+  //reset password
+  public resetPass(old: any, newPass: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    return this.http.patch(
+      `${this.apiUrl}/admin/reset_password/${JSON.stringify(newPass)}`,
+      old,
+      httpOptions
+    );
+  }
+
+  public login(password: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'responseType': 'text'
+      }),
+    };
+
+    return this.http.get(`${this.apiUrl}/admin/` + password);
   }
 }
