b83de47c3ecd668d9126c332b9ac910fb02ba1b8
rtiare <ttorosa55@gmail.com>
2021-03-04 20:37:30 -0800

    used session storage in http requests

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -9,35 +10,45 @@ import { Injectable } from '@angular/core';
 export class AdminService {
   //hold password for admin
   public adminPassword: any;
+  //hold keyName for
+  public adminKeyName: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+
     }),
   };
 
   //access service_requests for admins by id
   public searchById(id: any) {
     return this.http.get(
-      `${environment.apiUrl}/admin/${this.adminPassword}/service_requests/${id}`
+      `${environment.apiUrl}/admin/${sessionStorage.getItem(this.adminKeyName)}/service_requests/${id}`
     );
   }
 
   //display all service_requests in observable array
   public display(): Observable<any> {
     return this.http.get(
-      `${environment.apiUrl}/admin/${this.adminPassword}/service_requests`
+      `${environment.apiUrl}/admin/${sessionStorage.getItem(this.adminKeyName)}/service_requests`
     );
   }
 
   //reset password
   public resetPassword(old: any, newPass: any) {
     return this.http.patch(
-      `${environment.apiUrl}/admin/reset_password/${JSON.stringify(newPass)}`,
+      `${environment.apiUrl}/admin/reset_password/${newPass}`,
       old,
       this.httpOptions
     );
+  }
+
+  //logout user
+  public logOut() {
+    sessionStorage.removeItem(this.adminKeyName);
+    this.router.navigate(['/admin'])
+
   }
 }
