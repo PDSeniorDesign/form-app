ce955cc724609bba98e2e64ea0ab8ac51eb59194
Adrian Palomares <adrianpal45@gmail.com>
2021-03-08 20:05:34 -0800

    keeping track of checkbox value

--- src/app/form/employee-form/additional-information/additional-information.component.ts
+++ src/app/form/employee-form/additional-information/additional-information.component.ts
@@ -9,13 +9,29 @@ export class AdditionalInformationComponent implements OnInit {
   // Functions from parent component (employee-form)
   @Input() printForm;
   @Input() save;
+
   // These variable represent if the user wants access to these items
   internetAccess: boolean = false;
+  emailAccount: boolean = false;
+  emailEncryption: boolean = false;
+  laCountyAccess: boolean = false;
+  tokenlessAuth: boolean = false;
+  lacMobile: boolean = false;
+  cherwellSMS: boolean = false;
+  windowsManagement: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   sampleFunction() {
-    console.log(this.internetAccess);
+    console.log('Internet Access: ', this.internetAccess);
+    console.log('Email Account: ', this.emailAccount);
+    console.log('Email Encryption: ', this.emailEncryption);
+    console.log('La County Access: ', this.laCountyAccess);
+    console.log('Tokenless Auth: ', this.tokenlessAuth);
+    console.log('lacMobile: ', this.lacMobile);
+    console.log('Cherwell', this.cherwellSMS);
+    console.log('Windows: ', this.windowsManagement);
   }
 }
