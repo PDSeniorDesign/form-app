29cfe37f453817538367f91625591ca4eb0899c6
Adrian Palomares <adrianpal45@gmail.com>
2021-03-08 18:13:16 -0800

    keeping track of internet access state

--- src/app/form/employee-form/additional-information/additional-information.component.ts
+++ src/app/form/employee-form/additional-information/additional-information.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-additional-information',
   templateUrl: './additional-information.component.html',
-  styleUrls: ['./additional-information.component.css']
+  styleUrls: ['./additional-information.component.css'],
 })
 export class AdditionalInformationComponent implements OnInit {
+  // Functions from parent component (employee-form)
+  @Input() printForm;
+  @Input() save;
+  // These variable represent if the user wants access to these items
+  internetAccess: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  sampleFunction() {
+    console.log(this.internetAccess);
   }
-
 }
