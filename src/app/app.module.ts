c885c33125b7cc54b28b252cbd62e5e1fe224d27
PaulClefUbeCoding <72354974+PaulClefUbeCoding@users.noreply.github.com>
2021-03-11 04:01:42 -0800

    Making Contractor Submit Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -19,7 +24,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ContractorFormComponent } from './form/contractor-form/contractor-form.component';
-import { SubmitComponent } from './form/contractor-form/submit/submit.component';
 import { AdditionalInformationComponent } from './form/employee-form/additional-information/additional-information.component';
 import { EmployeeFormComponent } from './form/employee-form/employee-form.component';
 import { SubmitPageComponent } from './form/employee-form/submit-page/submit-page.component';
@@ -37,7 +41,6 @@ import { SharedModule } from './shared/shared.module';
     SubmitPageComponent,
     EmployeeFormComponent,
     ContractorFormComponent,
-    SubmitComponent,
     FormActionSelectionComponent,
     AdminComponent,
     AdminHeaderComponent,
@@ -55,6 +58,11 @@ import { SharedModule } from './shared/shared.module';
     MatListModule,
     MatStepperModule,
     MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatCardModule, 
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
