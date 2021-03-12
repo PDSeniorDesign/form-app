cba52347bb7f8abf4516fee374d81c48072bb4af
Adrian Palomares <adrianpal45@gmail.com>
2021-03-11 17:10:39 -0800

    added documentation and changed types

--- src/app/core/services/api-http.service.ts
+++ src/app/core/services/api-http.service.ts
@@ -49,22 +49,39 @@ export class ApiHttpService {
     return JSON.stringify(reformated);
   }
 
+  /**
+   * @description Use this function to create the form in the server. In other words, this function is called
+   * when the form is completed.
+   * @param data This is the value field of the FormGroup object. eg. FormGroup.value
+   * @returns An observable that will return the created form back from the server.
+   */
   public createForm(data: any): Observable<any> {
+    // TODO: This should set the completed field to complete
     return this.http.post(
       `${environment.apiUrl}/service_requests`,
       this.reformatDataPostEmployee(data),
       this.httpOptions
     );
   }
-
-  public getFormByRequestNumber(data: any): Observable<any> {
+  /**
+   * @description This function is used to get the form in the homescreen. Will basically just retrieve
+   * a specific form by it's request number.
+   * @param requestNumber The requestNumber is the number by which the forms are identified by. eg. the form's id.
+   * @returns An Observable the will contain the form object.
+   */
+  public getFormByRequestNumber(requestNumber: string): Observable<any> {
     return this.http.get(
-      `${environment.apiUrl}/service_requests/` + data,
+      `${environment.apiUrl}/service_requests/` + requestNumber,
       this.httpOptions
     );
   }
-
-  public saveForm(requestNumber: any, data: any): Observable<any> {
+  /**
+   * @description This function save's the form on the server.
+   * @param requestNumber The form's request number. This is used by the server to retrieve the form.
+   * @param data This is the data that we want to save.
+   * @returns An Observable with the update data. This is returned by the server.
+   */
+  public saveForm(requestNumber: string, data: object): Observable<any> {
     return this.http.put(
       `${environment.apiUrl}/service_requests/${requestNumber}`,
       this.reformatDataPostEmployee(data),
