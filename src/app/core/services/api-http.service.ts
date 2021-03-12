32be59c43ac729b02786ed46e32b83efa2d0e57a
Adrian Palomares <adrianpal45@gmail.com>
2021-03-11 16:59:44 -0800

    access info is now sent to the server as well

--- src/app/core/services/api-http.service.ts
+++ src/app/core/services/api-http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
@@ -12,43 +13,61 @@ export class ApiHttpService {
     }),
   };
 
-  //Reformats the data from the submit page to backend JSON compatible
-  public reformatDataPost(data: any) {
+  /**
+   * @description This method transforms the form group into the format that the server will accept.
+   * @param data This is the value field of the FormGroup. eg. FormGroup.value
+   * @returns A string representation of the json object that is accepted by the backend server.
+   */
+  public reformatDataPostEmployee(data: any): string {
     const reformated = {
+      // Personal Information
       lastName: data.personalInformation.lastName,
       firstName: data.personalInformation.firstName,
       middleInitial: data.personalInformation.middleInitial,
       employeeEmailAddress: data.personalInformation.emailAddress,
       businessPhoneNumber: data.personalInformation.phoneNumber,
+      // Address Information
       businessStreetAddress: data.addressInformation.address,
       businessCity: data.addressInformation.city,
       businessState: data.addressInformation.state,
       businessZip: data.addressInformation.zipCode,
+      // Employee Information
       employeeNumber: data.employeeInformation.employeeNumber,
       hostedId: data.employeeInformation.hostedId,
+      // Access Information
+      ibmLogOnId: data.accessInformation.ibmLogonId,
+      majorGroupCode: data.accessInformation.majorGroupCode,
+      lsoGroupCode: data.accessInformation.lsoGroupCode,
+      securityAuthorization: data.accessInformation.securityAuthorization,
+      unixLogOnId: data.accessInformation.unixLogonId,
+      unixApplication: data.accessInformation.application,
+      unixAccessGroup: data.accessInformation.accessGroup,
+      unixAccountNumber: data.accessInformation.accountNumber,
+      billingAccountNumber: data.accessInformation.billingAccountNumber,
+      // FIXME: On server side accessType(securid) might be misssing
     };
     return JSON.stringify(reformated);
   }
 
-  public createForm(data: any) {
+  public createForm(data: any): Observable<any> {
     return this.http.post(
       `${environment.apiUrl}/service_requests`,
-      this.reformatDataPost(data),
+      this.reformatDataPostEmployee(data),
       this.httpOptions
     );
   }
 
-  public getFormByRequestNumber(data: any) {
+  public getFormByRequestNumber(data: any): Observable<any> {
     return this.http.get(
       `${environment.apiUrl}/service_requests/` + data,
       this.httpOptions
     );
   }
 
-  public saveForm(requestNumber: any, data: any) {
+  public saveForm(requestNumber: any, data: any): Observable<any> {
     return this.http.put(
       `${environment.apiUrl}/service_requests/${requestNumber}`,
-      this.reformatDataPost(data),
+      this.reformatDataPostEmployee(data),
       this.httpOptions
     );
   }
