293f837e41ea79ac78e86efa3d59879a3614e711
rtiare <ttorosa55@gmail.com>
2021-03-12 18:35:46 -0800

    Changed text to password for password input

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -8,6 +8,8 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AdminService {
+  //successful alert
+  loginAlert: boolean = false;
   //hold password for admin
   public adminPassword: any;
   //hold keyName for
