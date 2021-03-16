755dd982ebc005cdd5fe54c23f4a8752bad60c71
rtiare <ttorosa55@gmail.com>
2021-03-15 22:16:40 -0700

    Put Admin login header in seperate component

---
+++ src/app/admin-login-header/admin-login-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminLoginHeaderComponent } from './admin-login-header.component';
+
+describe('AdminLoginHeaderComponent', () => {
+  let component: AdminLoginHeaderComponent;
+  let fixture: ComponentFixture<AdminLoginHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminLoginHeaderComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminLoginHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/admin-login-header/admin-login-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login-header',
+  templateUrl: './admin-login-header.component.html',
+  styleUrls: ['./admin-login-header.component.css']
+})
+export class AdminLoginHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
