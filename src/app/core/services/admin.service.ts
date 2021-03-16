1b0c0e392eb0e3f8a9e7c5093a34986879bf4dcf
Adrian Palomares <adrianpal45@gmail.com>
2021-03-16 16:08:22 -0700

    fixed lint errors

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -1,70 +1,72 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminService {
-  //tranferring form data in admin side between components
+  // tranferring form data in admin side between components
   public adminFormData: any;
-  //hold password for admin
+  // hold password for admin
   public adminPassword: any;
-  //hold keyName for
+  // hold keyName for
   public adminKeyName: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
-  //access service_requests for admins by id
-  public searchById(id: any) {
-    //set the password in headers
-    var httpOptions = {
+  // access service_requests for admins by id
+  public searchById(id: any): Observable<any> {
+    // set the password in headers
+    const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'password': this.adminPassword,
+        password: this.adminPassword,
       }),
     };
 
     return this.http.get(
-      `${environment.apiUrl}/admin/service_requests/${id}`, httpOptions
+      `${environment.apiUrl}/admin/service_requests/${id}`,
+      httpOptions
     );
   }
 
-  //display all service_requests in observable array
+  // display all service_requests in observable array
   public display(): Observable<any> {
-    var httpOptions = {
+    const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'password': this.adminPassword,
+        password: this.adminPassword,
       }),
     };
     return this.http.get(
-      `${environment.apiUrl}/admin/service_requests`, httpOptions
+      `${environment.apiUrl}/admin/service_requests`,
+      httpOptions
     );
   }
 
-  //reset password
-  public resetPassword(old: string, newPass: string) {
-
-    var httpOptions = {
+  // reset password
+  public resetPassword(old: string, newPass: string): Observable<any> {
+    const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'password': old,
+        password: old,
         'new-password': newPass,
       }),
     };
 
     return this.http.patch(
-      `${environment.apiUrl}/admin/reset_password`, httpOptions, httpOptions
+      `${environment.apiUrl}/admin/reset_password`,
+      httpOptions,
+      httpOptions
     );
   }
 
-  //logout user
-  public logOut() {
+  // logout user
+  public logOut(): void {
     sessionStorage.removeItem(this.adminKeyName);
-    this.router.navigate(['/admin'])
-
+    this.router.navigate(['/admin']);
   }
 }
