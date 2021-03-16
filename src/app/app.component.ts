52d0a1076d2fc65fa473dfe52b9c4757c2963384
rtiare <ttorosa55@gmail.com>
2021-03-15 22:29:36 -0700

    erased few variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router, NavigationEnd } from '@angular/router';
 
@@ -8,24 +9,29 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  @Input() currentPage;
+  @Input() returnUrl;
   title = 'form-app';
   registrationForm: FormGroup;
-
-
   showheader: boolean;
-
-  //if navigation to login page is successful, then don't show header for admin functions
+  
+  //if navigation to admin login page is successful, then don't show normal header for admin functions
   constructor (private router: Router) {
+    
     this.router.events.subscribe((event: any) => {
       if (event instanceof NavigationEnd) {
-        //if register or login page navigated, dont show
         if (event.url === '/admin' || event.url == '/admin/service-requests' ||
          event.url == '/admin/reset-password' 
-         || event.url == '/admin/review-request' || event.url == '/admin/service-request-detail') {
+         || event.url == '/admin/review-request' || event.url == '/admin/service-request-detail'
+         || event.url == '/admin/review-employee' 
+         )  {
           this.showheader= false;
-        } else {
+
+        }
+        else {
           this.showheader= true;
         }
+
       }
     });
   }
