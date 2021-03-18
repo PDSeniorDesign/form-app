0959e60d0b9074090f80ac08f5c4e97b9fd6a321
rtiare <ttorosa55@gmail.com>
2021-03-18 15:49:04 -0700

    Created formGroups for approvers

--- src/app/admin/review-employee/review-employee.component.ts
+++ src/app/admin/review-employee/review-employee.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-review-employee',
   templateUrl: './review-employee.component.html',
-  styleUrls: ['./review-employee.component.css']
+  styleUrls: ['./review-employee.component.css'],
 })
 export class ReviewEmployeeComponent implements OnInit {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
 
-  constructor() { }
+  //approval FormGroup-manager, divisionChief, etc
+  approval: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    //create the form group
+    this.approval = new FormGroup({
+      manager: new FormGroup({
+        name: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+      }),
+      departmentHead: new FormGroup ({
+        name: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+      }),
+      applicationCoord: new FormGroup({
+        name: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+      }),
+      divisionChief: new FormGroup({
+        name: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+      }),
+      deptInfoSecurity: new FormGroup({
+        name: new FormControl(''),
+        phone: new FormControl(''),
+        email: new FormControl(''),
+      }),
+    });
   }
 
+  print():void {
+    console.log(this.approval.value);
+  }
+
+  closeAllPanels() {
+    this.accordion.closeAll();
+  }
+  openAllPanels() {
+    this.accordion.openAll();
+  }
 }
