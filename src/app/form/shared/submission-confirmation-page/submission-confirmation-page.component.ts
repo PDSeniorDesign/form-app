dc1f843d67083fa8ee4a3add269ebf0e731d03fb
Adrian Palomares <adrianpal45@gmail.com>
2021-03-23 13:36:06 -0700

    moved submission confirmation page to its own component

--- src/app/form/shared/submission-confirmation-page/submission-confirmation-page.component.ts
+++ src/app/form/shared/submission-confirmation-page/submission-confirmation-page.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-submission-confirmation-page',
   templateUrl: './submission-confirmation-page.component.html',
-  styleUrls: ['./submission-confirmation-page.component.css']
+  styleUrls: ['./submission-confirmation-page.component.css'],
 })
 export class SubmissionConfirmationPageComponent implements OnInit {
+  /**
+   * The request number of the service request(form).
+   */
+  @Input() requestNumber: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
