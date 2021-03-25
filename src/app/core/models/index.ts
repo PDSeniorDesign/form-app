d30b7239825c95d9b672c4bf92f00446a25e8254
rawadmoussa <61433797+rawadmoussa@users.noreply.github.com>
2021-03-25 10:30:51 -0700

    Delete src directory

--- src/app/core/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './form-groups.model';
-export * from './EmployeeFormDTO';
--- src/app/form/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './employee-form/employee-form.component';
-export * from './contractor-form/contractor-form.component';
