4ced1fd2807f32457b99c7a8e315b648d302174e
rtiare <ttorosa55@gmail.com>
2021-03-27 18:49:58 -0700

    created arrays to add div/dept. head, etc

--- src/app/admin/approvers/approvers.component.ts
+++ src/app/admin/approvers/approvers.component.ts
@@ -10,12 +10,12 @@ import { AdminService } from 'src/app/core/services/admin.service';
 })
 export class ApproversComponent implements OnInit {
   //four arrays
-  divChief: Array<any>;
-  deptInfo: Array<any>;
-  deptHead: Array<any>;
-  appCoord: Array<any>;
+  divChief: Array<any> = [];
+  deptInfo: Array<any> = [];
+  deptHead: Array<any> = [];
+  appCoord: Array<any> = [];
 
-  allApprover: Array<any>;
+  allApprover: Array<any> = [];
 
   //add user
   newApprover: FormGroup;
@@ -38,12 +38,15 @@ export class ApproversComponent implements OnInit {
 
   constructor(private adminService: AdminService, private router: Router) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.newApprover = new FormGroup({
       name: new FormControl(null, [Validators.pattern('[a-z A-Z]*')]),
       phone: new FormControl(null, [Validators.pattern('[0-9]{10}')]),
       email: new FormControl(null, [Validators.email]),
     });
+    this.allApprover = []
+    await this.addToArray();
+    await console.log(this.allApprover);
   }
 
   applyFilter(event: Event) {
@@ -51,9 +54,40 @@ export class ApproversComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  ///get all div. cheif, app_coord
+  //add div cheif to div array
+  //add div chief to all array
+  //show allarray to table
+
+  //add to each individual array, then concat for main table
+  addToArray(): void {
+    this.adminService.getAllDivChief().subscribe((res) => {
+      this.divChief = res;
+      //console.log(this.divChief);
+      
+    });
+    this.adminService.getAllDeptHead().subscribe((res) => {
+      this.deptHead = res;
+      
+    });
+    this.adminService.getAllAppCoord().subscribe((res) => {
+      this.appCoord = res;
+      //this.allApprover.push(res);
+    });
+    this.adminService.getAllDeptInfoSec().subscribe((res) => {
+      this.deptInfo = res;
+      //this.allApprover.push(res);
+      this.allApprover = this.divChief.concat(this.deptHead).concat(this.appCoord).concat(this.deptInfo);
+      console.log(this.allApprover);
+    });
+  }
+
+
+  //for edditing. check what role column it is, then call patch function
 
   clear(): void {
     this.newApprover.reset();
+    this.addToArray();
   }
   //based on selection: do http.post
   add(): void {
@@ -65,7 +99,6 @@ export class ApproversComponent implements OnInit {
           this.sucess = true;
           console.log(res);
         });
-
     } else if (this.selected == this.roles[1]) {
       this.adminService
         .postDeptHead(this.newApprover.value)
@@ -73,7 +106,6 @@ export class ApproversComponent implements OnInit {
           this.sucess = true;
           console.log(res);
         });
-
     } else if (this.selected == this.roles[2]) {
       this.adminService
         .postAppCoord(this.newApprover.value)
@@ -81,7 +113,6 @@ export class ApproversComponent implements OnInit {
           this.sucess = true;
           console.log(res);
         });
-
     } else if (this.selected == this.roles[3]) {
       this.adminService
         .postDeptInfoSec(this.newApprover.value)
