a092f4b37aeab01ee01f2f5d6ab11c60122fc102
Adrian Palomares <adrianpal45@gmail.com>
2021-04-02 20:56:10 -0700

    added internet access step

---
+++ src/app/form/employee-form/internet-access/internet-access.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internet-access',
+  templateUrl: './internet-access.component.html',
+  styleUrls: ['./internet-access.component.css']
+})
+export class InternetAccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
