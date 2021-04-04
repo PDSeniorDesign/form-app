5e29c32c4354814ea26c679f527a5935678808cb
rtiare <ttorosa55@gmail.com>
2021-04-03 19:12:26 -0700

    created new component to implement admin contractor form

--- src/app/admin/review-contractor/review-contractor.component.spec.ts
+++ src/app/admin/review-contractor/review-contractor.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ReviewRequestComponent } from './review-request.component';
+import { ReviewContractorComponent } from './review-contractor.component';
 
-describe('ReviewRequestComponent', () => {
-  let component: ReviewRequestComponent;
-  let fixture: ComponentFixture<ReviewRequestComponent>;
+describe('ReviewContractorComponent', () => {
+  let component: ReviewContractorComponent;
+  let fixture: ComponentFixture<ReviewContractorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReviewRequestComponent ]
+      declarations: [ ReviewContractorComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ReviewRequestComponent);
+    fixture = TestBed.createComponent(ReviewContractorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
